85ea28e832743d69f45c2656623c1c18e8569021
Iain <iain.coffield@gmail.com>
2021-12-02 10:28:18 -0600

    Day 2: Initial Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,17 @@
 import days.Day01
+import days.Day02
 
 fun main() {
     println("Welcome to Advent of Code 2021!")
+    println()
 
     val days = listOf(
-        Day01()
+        Day01(),
+        Day02(),
     )
 
-    days.forEach { it.run() }
+    days.forEach {
+        it.run()
+        println()
+    }
 }
---
+++ src/main/kotlin/days/Day02.kt
@@ -0,0 +1,57 @@
+package days
+
+import util.FileUtil.getInput
+
+fun main() {
+    Day02().run()
+}
+
+class Day02: Day {
+    override fun run() {
+        val input = getInput("Day02.txt")
+        val commands = input
+            .map { it.trim() }
+            .map { it.split(" ") }
+            .map { (command, value) -> Command(DIRECTION.valueOf(command.uppercase()), value.toInt()) }
+
+        println("Results for Day One:")
+        println("Part One: ${partOne(commands)}")
+        println("Part Two: ${partTwo(commands)}")
+    }
+
+    private fun partOne(commands: List<Command>): Int {
+        val submarine = commands.fold(Submarine()) { sub, command -> sub.executeCommand(command)}
+        return submarine.horizontal * submarine.depth
+    }
+
+    private fun partTwo(commands: List<Command>): Int {
+        val submarine = commands.fold(Submarine()) { sub, command -> sub.executeCommandWithAim(command)}
+        return submarine.horizontal * submarine.depth
+    }
+}
+
+enum class DIRECTION {
+    FORWARD,
+    DOWN,
+    UP
+}
+
+data class Command(val direction: DIRECTION, val value: Int)
+
+data class Submarine(val horizontal: Int = 0, val depth: Int = 0, val aim: Int = 0) {
+    fun executeCommand(command: Command): Submarine {
+        return when(command.direction) {
+            DIRECTION.FORWARD -> copy(horizontal = horizontal + command.value)
+            DIRECTION.DOWN -> copy(depth = depth + command.value)
+            DIRECTION.UP -> copy(depth = depth - command.value)
+        }
+    }
+
+    fun executeCommandWithAim(command: Command): Submarine {
+        return when(command.direction) {
+            DIRECTION.FORWARD -> copy(horizontal = horizontal + command.value, depth = depth + aim * command.value)
+            DIRECTION.DOWN -> copy(aim = aim + command.value)
+            DIRECTION.UP -> copy(aim = aim - command.value)
+        }
+    }
+}
