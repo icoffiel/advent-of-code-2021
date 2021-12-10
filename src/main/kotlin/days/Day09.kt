fdfce406547c80f11972187ce2d8dc27ce8fc480
Iain <iain.coffield@gmail.com>
2021-12-09 20:25:22 -0600

    Day 9 Refactoring

--- src/main/kotlin/days/Day09.kt
+++ src/main/kotlin/days/Day09.kt
@@ -18,59 +18,48 @@ class Day09 : Day {
     }
 
     private fun partOne(sanitizedInput: List<String>): Int {
-        val cave = sanitizedInput
-            .map { it.toIntList() }
+        val cave = sanitizedInput.map { it.toIntList() }
 
-        val columns = cave.size
-        val rows = cave[0].size
-
-        val lowPointValues = (0 until columns)
-            .map { y ->
-                (0 until rows)
-                    .mapNotNull { x ->
-                        val pointToCheck = Point(x, y)
-                        if (cave.isLowPoint(pointToCheck)) {
-                            cave[pointToCheck.y][pointToCheck.x]
-                        } else {
-                            null
+        val lowPointValues =
+            cave.indices
+                .flatMap { y ->
+                    cave[0].indices
+                        .mapNotNull { x ->
+                            val pointToCheck = Point(x, y)
+                            if (cave.isLowPoint(pointToCheck)) {
+                                cave[pointToCheck]
+                            } else {
+                                null
+                            }
                         }
-                    }
-            }
+                }
 
-        return lowPointValues
-            .flatten()
-            .sumOf { it + 1 }
+        return lowPointValues.sumOf { it + 1 }
     }
 
     private fun partTwo(sanitizedInput: List<String>): Int {
-        val cave = sanitizedInput
-            .map { it.toIntList() }
+        val cave = sanitizedInput.map { it.toIntList() }
 
-        val columns = cave.size
-        val rows = cave[0].size
-
-        val lowPointCoordinates = (0 until columns)
-            .map { y ->
-                (0 until rows)
-                    .mapNotNull { x ->
-                        val pointToCheck = Point(x, y)
-                        if (cave.isLowPoint(pointToCheck)) {
-                            pointToCheck
-                        } else {
-                            null
+        val lowPointCoordinates =
+            cave.indices
+                .flatMap { y ->
+                    cave[0].indices
+                        .mapNotNull { x ->
+                            val pointToCheck = Point(x, y)
+                            if (cave.isLowPoint(pointToCheck)) {
+                                pointToCheck
+                            } else {
+                                null
+                            }
                         }
-                    }
-            }
-            .flatten()
+                }
 
-        val basins = lowPointCoordinates
-            .map { cave.basin(it) }
+        val basins = lowPointCoordinates.map { cave.basin(it) }
 
         return basins
             .map { it.size }
             .sorted()
-            .reversed()
-            .take(3)
+            .takeLast(3)
             .reduce { acc, size -> acc * size }
     }
 }
@@ -82,22 +71,28 @@ fun String.toIntList(): List<Int> =
 
 fun List<List<Int>>.isLowPoint(coOrdinate: Point): Boolean {
     return validSurroundingPoints(coOrdinate)
-        .map { this[it.y][it.x] }
-        .none { it <= this[coOrdinate.y][coOrdinate.x] }
+        .map { this[it] }
+        .none { it <= this[coOrdinate] }
 }
 
 private fun List<List<Int>>.validSurroundingPoints(coOrdinate: Point): List<Point> {
-    val columns = this.size
-    val rows = this[0].size
+    return coOrdinate
+        .neighbours()
+        .filter { it in this }
+}
 
+operator fun List<List<Int>>.contains(point: Point): Boolean =
+    point.y in this.indices && point.x in this[point.y].indices
+
+operator fun List<List<Int>>.get(point: Point): Int = this[point.y][point.x]
+
+fun Point.neighbours(): List<Point> {
     return listOf(
-        Point(coOrdinate.x - 1, coOrdinate.y),
-        Point(coOrdinate.x + 1, coOrdinate.y),
-        Point(coOrdinate.x, coOrdinate.y - 1),
-        Point(coOrdinate.x, coOrdinate.y + 1),
+        Point(this.x - 1, this.y),
+        Point(this.x + 1, this.y),
+        Point(this.x, this.y - 1),
+        Point(this.x, this.y + 1),
     )
-        .filter { it.x in 0 until rows }
-        .filter { it.y in 0 until columns }
 }
 
 fun List<List<Int>>.basin(coOrdinate: Point): List<Point> {
@@ -111,7 +106,7 @@ fun List<List<Int>>.basin(coOrdinate: Point): List<Point> {
             .map { this.validSurroundingPoints(it) }
             .flatten()
             .filter { !foundPoints.contains(it) }
-            .filter { this[it.y][it.x] != boundaryHeight }
+            .filter { this[it] != boundaryHeight }
 
         if (newPoints.isEmpty()) {
             return foundPoints.toList()
