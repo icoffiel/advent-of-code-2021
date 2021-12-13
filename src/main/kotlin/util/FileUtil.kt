389a59c21f7ca2856acbb59921fbec064b495465
Iain <iain.coffield@gmail.com>
2021-12-13 15:55:16 -0600

    feat: Moving files to `resources/inputs`

--- src/main/kotlin/util/FileUtil.kt
+++ src/main/kotlin/util/FileUtil.kt
@@ -3,6 +3,6 @@ package util
 import java.io.File
 
 object FileUtil {
-    fun getInput(file: String, path: String = "src/main/kotlin/days") = File("$path/$file").readLines()
-    fun getInputString(file: String, path: String = "src/main/kotlin/days") = File("$path/$file").readText()
+    fun getInput(file: String, path: String = "src/main/resources/inputs") = File("$path/$file").readLines()
+    fun getInputString(file: String, path: String = "src/main/resources/inputs") = File("$path/$file").readText()
 }
